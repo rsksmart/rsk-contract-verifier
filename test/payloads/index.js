b827ba061a66741b21da20fa79f392102463cead
emi <emiliorizzo@gmail.com>
2019-09-23 16:09:37 +0000

    Update tests

--- test/payloads/index.js
+++ test/payloads/index.js
@@ -6,4 +6,5 @@ import ADDRESS_LINKER from './AddressLinker.json'
 import CUCKOO from './cuckoo.json'
 import RESOLVER from './MultiChainResolver.json'
 import REGISTRAR from './Registrar.json'
-export default { ERC20, ERC721, RIF, RIFF, ADDRESS_LINKER, CUCKOO, RESOLVER, REGISTRAR }
+import SELF_DESTRUCT from './SelfDestruct.json'
+export default { ERC20, ERC721, RIF, RIFF, ADDRESS_LINKER, CUCKOO, RESOLVER, REGISTRAR, SELF_DESTRUCT }
--- test/verifyFromPayload.spec.js
+++ test/verifyFromPayload.spec.js
@@ -7,7 +7,6 @@ describe(`# Verify from payload`, () => {
   for (let key in payloads) {
     let payload = payloads[key]
     let _expected = payload._expected || {}
-    let { warnings } = _expected
     describe(`# contract ${key}`, function () {
       this.timeout(20000)
       it('should verify a contract from payload', async () => {
@@ -17,8 +16,11 @@ describe(`# Verify from payload`, () => {
         expect(verification).has.ownProperty('resultBytecodeHash')
         expect(verification).has.ownProperty('usedSettings')
         expect(verification).has.ownProperty('usedLibraries')
+        expect(verification).has.ownProperty('decodedMetadata')
         expect(verification.errors).to.be.equal(undefined)
-        if (warnings) expect(verification.warnings).to.be.deep.equal(warnings)
+        for (let e in _expected) {
+          expect(verification[e]).to.be.deep.equal(_expected[e])
+        }
         expect(verification.bytecodeHash, 'hashes').to.be.deep.equal(verification.resultBytecodeHash)
         expect(verification.usedLibraries).to.be.deep.equal(payload.libraries)
       })
