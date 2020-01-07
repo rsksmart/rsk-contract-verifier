f351619097360e54190b98ec2cf7945b17261f60
emi <emiliorizzo@gmail.com>
2020-01-07 22:51:37 +0100

    Build 0.0.7

--- dist/lib/verifier.js
+++ dist/lib/verifier.js
@@ -119,7 +119,7 @@ function verifyResults(contractName, bytecode, evm, deployedBytecode, libs) {
 }
 
 function removeMetadata(bytecode, metadata) {
-  const metadataStart = bytecode.indexOf(metadata);
+  const metadataStart = metadata.length;
   return metadataStart > 0 ? bytecode.substr(0, metadataStart) : bytecode;
 }
 
