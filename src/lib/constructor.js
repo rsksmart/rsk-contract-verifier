69f5892a212e3864fb90a9a763f340d54ce2b6a8
emi <emiliorizzo@gmail.com>
2020-12-28 12:34:43 -0700

    Change lib/constructor, abiEncoder
    - Replace ethereumjs-abi with @ethersproject/abi

--- src/lib/constructor.js
+++ src/lib/constructor.js
@@ -1,6 +1,16 @@
 import { toBuffer, add0x, remove0x, isAddress } from '@rsksmart/rsk-utils'
-import { rawEncode as encode, rawDecode as decode } from 'ethereumjs-abi'
+import { AbiCoder } from '@ethersproject/abi'
 import { isBN } from 'bn.js'
+
+const abiCoder = new AbiCoder(function (type, value) {
+  if (type.match(/^u?int/) && !Array.isArray(value) && (value !== Object(value) || value.constructor.name !== 'BN' || isBN(value))) {
+    return value.toString()
+  }
+  return value
+})
+
+const encode = (types, value) => remove0x(abiCoder.encode(types, value))
+const decode = (types, value) => abiCoder.decode(types, value, true)
 
 export const getConstructorAbi = abi => abi.filter(x => x.type === 'constructor')[0]
 
@@ -10,14 +20,20 @@ export const getConstructorTypes = abi => getTypesFromAbi(getConstructorAbi([...
 
 export const encodeConstructorArgs = (args, abi) => {
   const types = getConstructorTypes(abi)
-  const encoded = encode(types, remove0x(args))
+  for (let p in types) {
+    let type = types[p]
+    let value = args[p]
+    if (type.indexOf('bytes') > -1 && !value) value = '0x'
+    args[p] = value
+  }
+  const encoded = encode(types, args)
   return encoded.toString('hex')
 }
 
 export const normalizeOutput = out => {
   if (Array.isArray(out)) return out.map(normalizeOutput)
   if (isBN(out)) out = add0x(out.toString(16))
-  if (isAddress(out)) out = add0x(out)
+  if (isAddress(out)) out = add0x(out.toLowerCase())
   return out
 }
 
