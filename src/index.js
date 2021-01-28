6705972592c4692284f970498ffc413c273fa7f0
emi <emiliorizzo@gmail.com>
2021-01-28 12:26:47 -0700

    Upate exports add isVerified

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { cacheSolc } from './lib/cacheSolc'
 import * as solidityMetadata from './lib/solidityMetadata'
 import * as config from './lib/config'
-export { Verifier } from './lib/verifier'
+export { Verifier, isVerified } from './lib/verifier'
 export { verifyParams } from './lib/verifyFromPayload'
 export { Compiler } from './lib/compiler'
 export { solidityMetadata, cacheSolc, config }
